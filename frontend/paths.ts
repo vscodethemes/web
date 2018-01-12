3d546ae75f4f5e528b9e3a6cf7a6a25f3d8c12ae
Jordan Schroter <jordan.schroter@gmail.com>
2018-01-11 21:50:48 -0500

    Webpack config expects output path to be absolute

--- frontend/paths.ts
+++ frontend/paths.ts
@@ -9,7 +9,8 @@ export const entry = isProduction
     path.resolve(__dirname, './index.ts')
 
 export const output = isProduction
-  ? // Path doesn't really matter because we ouput to memory.
-    'build'
+  ? // Path doesn't matter because we ouput to memory in production
+    // but still needs to be an abolute path to be a valid webpack config.
+    path.resolve(__dirname, 'build')
   : // Output to build directory in development.
     path.resolve(__dirname, '../build')
