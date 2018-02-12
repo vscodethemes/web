48eb875f744fadd608f605f742cdc9c3ac0f84b9
Jordan Schroter <github@jschr.io>
2018-02-11 21:04:29 -0500

    Prevent zoom in on input focus iOS

--- frontend/components/Document.tsx
+++ frontend/components/Document.tsx
@@ -74,8 +74,11 @@ export default function Document(props: DocumentProps) {
         )}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <meta
           name="description"
           content="Preview themes from the VSCode marketplace."
