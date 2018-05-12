9df403bdb3663549ce8668514265b1d502faff65
Jordan Schroter <github@jschr.io>
2018-05-11 20:16:53 -0400

    Added tokenizer package

---
+++ tokenizer/languages/index.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+import * as path from 'path'
+
+export const typescript = path.resolve(__dirname, './typescript.xml')
+// VSCode uses the typescript grammar for javascript.
+export const javascript = path.resolve(__dirname, './typescript.xml')
