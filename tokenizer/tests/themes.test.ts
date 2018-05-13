f7dcd170f9a23dfd5b973ba4e7f1ede4ce721cdb
Jordan Schroter <github@jschr.io>
2018-05-12 20:01:08 -0400

    Set default foreground theme setting color from editor foreground

--- tokenizer/tests/themes.test.ts
+++ tokenizer/tests/themes.test.ts
@@ -1,19 +1,19 @@
 import * as fs from 'fs'
-import Tokenizer from '../'
+import Tokenizer from '../index'
 import { readJSON, templates } from './paths'
 
 // These tests cannot be in a separate file because of an internal lib
 // that uses Object.defineProperty. If Tokenizer is imported a second time,
 // calling Object.defineProperty will cause an error.
 
-const themes = ['palenight', 'nord', 'hopscotch']
+const themes = ['bimbo', 'palenight', 'nord', 'hopscotch']
 const languages = ['javascript']
 
 themes.forEach(theme => {
   languages.forEach(lang => {
     test(`${theme}-${lang}`, () => {
-      const { tokenColors: themeSettings } = readJSON(`${theme}/${theme}.json`)
-      const tokenizer = new Tokenizer(themeSettings, lang)
+      const themeDefinition = readJSON(`${theme}/${theme}.json`)
+      const tokenizer = new Tokenizer(themeDefinition, lang)
       const lineTokens = tokenizer.tokenizeText((templates as any)[lang])
       expect(lineTokens).toEqual(readJSON(`${theme}/${theme}-${lang}.json`))
     })
