caf498d5b8cd8dcbe3afb71e22bdaa14d6e826cb
Jordan Schroter <github@jschr.io>
2018-05-19 16:38:46 -0400

    Added templates package

---
+++ templates/index.ts
@@ -0,0 +1,8 @@
+import { LanguageOptions } from '@vscodethemes/types'
+import jsTemplate from './javascript'
+
+export default {
+  [LanguageOptions.javascript]: jsTemplate,
+  [LanguageOptions.css]: '',
+  [LanguageOptions.html]: '',
+}
