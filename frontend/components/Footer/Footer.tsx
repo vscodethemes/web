be91e202bb132b28136718e0cf508810f4dd7ffd
Jordan Schroter <github@jschr.io>
2018-07-19 01:29:20 -0400

    Removed footer twitter link

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -3,22 +3,15 @@ import { Icon, Icons } from '../'
 import AlgoliaLogo from './AlgoliaLogo'
 import styles from './Footer.styles'
 
-const githubLink = 'https://github.com/jschr/vscodethemes'
-const twitterLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-  'Preview themes from the VSCode marketplace',
-)}&via=_jschr&url=${encodeURIComponent(
-  'https://vscodethemes.com',
-)}&hashtags=vscode`
-
 const Footer: React.SFC<{}> = () => (
   <div className={styles.footer}>
     <div className={styles.container}>
       <div className={styles.links}>
-        <a className={styles.link} href={githubLink}>
+        <a
+          className={styles.link}
+          href="'https://github.com/jschr/vscodethemes'"
+        >
           <Icon className={styles.icon} icon={Icons.github} />View on Github
-        </a>
-        <a className={styles.link} href={twitterLink} target="_blank">
-          <Icon className={styles.icon} icon={Icons.twitter} /> Share on Twitter
         </a>
       </div>
       <AlgoliaLogo />
