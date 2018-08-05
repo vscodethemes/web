ad251fa41b20eca77cdd0bc4da306298ec8820d8
Jordan Schroter <github@jschr.io>
2018-08-05 14:29:26 -0400

    Prepend host to urls, fix optional repo info

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,7 @@ module.exports = require('@zeit/next-typescript')({
     algoliaAppId: process.env.TF_VAR_algolia_app_id,
     algoliaIndex: process.env.TF_VAR_algolia_index,
     algoliaSearchKey: process.env.ALGOLIA_SEARCH_KEY,
+    host: process.env.HOST,
   },
   onDemandEntries: {
     // Cache all pages, fixes "Invalid element type error" that occurs when
