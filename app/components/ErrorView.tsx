dd3bc1dce7a04a7d1537f9533b38810ff8bffcff
Jordan Schroter <github@jschr.io>
2024-10-22 09:53:47 -0400

    Clean repo

--- app/components/ErrorView.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export default function ErrorView({ children }: React.PropsWithChildren<{}>) {
-  return <main className="error-view">{children}</main>;
-}
--- app/components/Spacer.tsx
+++
@@ -1,3 +0,0 @@
-export default function Spacer() {
-  return <div className="spacer" />;
-}
